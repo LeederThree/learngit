47838cd9ddc6f85fc788d10e41f5bf50eff7eda7
LeederThree <74589239+LeederThree@users.noreply.github.com>
2022-09-21 22:50:15 +0800

    力扣203：移除链表元素

---
+++ 203_removeElements.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: ListNode
+        :type val: int
+        :rtype: ListNode
+        """
+        prevNode = ListNode(-1)
+        prevNode.next = head
+        prev = prevNode
+        while prev.next:
+            if prev.next.val == val:
+                prev.next = prev.next.next
+            else: prev = prev.next
+        return prevNode.next
